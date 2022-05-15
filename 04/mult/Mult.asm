49e48aad88891e20177cf4d54a512e2c9863a1eb
Fabian-Faraz Farid <fabianfarid@live.de>
2022-05-15 12:40:06 +0200

    Solved 04 - Mult

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -9,4 +9,25 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@R2
+M=0
+
+(LOOP)
+@R1
+D=M
+@ENDPROGRAM
+D;JEQ
+
+@R0
+D=M
+@R2
+M=D+M
+@R1
+M=M-1
+
+@LOOP
+0;JMP
+
+(ENDPROGRAM)
+@ENDPROGRAM
+0;JMP
